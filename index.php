65c3d9441601f09f27deeb9b3aa0c3216cda4be2
Przemysław Kopciara <przemyslawkopciara@gmail.com>
2019-12-01 22:24:43 +0100

    Added auto refresh meta tag

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta http-equiv="refresh" content="30">
 	<title>Random Quotes</title>
 	<link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="css/normalize.css">
